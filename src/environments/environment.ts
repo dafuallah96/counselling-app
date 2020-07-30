5cd4cb982ff74564ab687966c9d6fc97a29401a9
Dafuallah <32087554+dafuallah96@users.noreply.github.com>
2020-07-30 17:02:33 +0800

    Removede credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,6 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDBd3r0mYqYsybFSccQhZpybZ71r-IoTaI",
-    authDomain: "counselling-platform.firebaseapp.com",
-    databaseURL: "https://counselling-platform.firebaseio.com",
-    projectId: "counselling-platform",
-    storageBucket: "counselling-platform.appspot.com",
-    messagingSenderId: "717950788774",
-    appId: "1:717950788774:web:78a54eedd2f9d9d2260067",
-    measurementId: "G-5CZ1466PT9"
   }
 };
 
